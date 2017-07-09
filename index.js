3dac708393742d936e8e71a3f8e43160e4e006e6
胡霆 <huting9386@gmail.com>
2017-07-09 23:22:26 +0800

    爬取对应站点数据形成对象数组完成

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
-var express = require('express');
-var app = express();
+var eventproxy = require('eventproxy');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+var url = require('url');
+var segmentfault = 'https://segmentfault.com';
+var segmentfaultUrl = 'https://segmentfault.com/news?page=';
 
-app.get('/', function(req, res) {
-  res.send('hello, express');
-});
+// 得到一个 eventproxy 的实例
+var ep = new eventproxy();
 
-app.listen(3000);
+var urls = [];
+for(let i = 1; i < 6; i++) {
+  urls.push('https://segmentfault.com/news?page=' + i);
+}
+
+urls.forEach(function (newUrl, index) {
+      superagent.get(newUrl)
+        .end(function (err, res) {
+          console.log('fetch ' + newUrl + ' successful');
+          ep.emit('new_html', [newUrl, res.text]);
+        });
+    });
+
+
+ep.after('new_html', urls.length, (news => {
+       let newList = [];
+       news.forEach(topicPair => {
+        let topicUrl = topicPair[0];
+        let topicHtml = topicPair[1];
+        let $ = cheerio.load(topicHtml);
+        $('.news__item.stream__item').each((idx, element) => {
+          let $element = $(element);    
+          let news2 = {
+            num: $element.find('.stream__item-zan-number').text().trim(),
+            title: $element.find('.news__item-title a').text().trim(),
+            href: url.resolve(segmentfault, $element.find("h4 a").attr('href')),
+            author: $element.find('.news__item-meta span a').eq(0).text().trim()
+          };
+          newList.push(news2);
+        });
+      });
+      console.log('final:');
+      console.log(newList);
+      process.exit();
+    }));
